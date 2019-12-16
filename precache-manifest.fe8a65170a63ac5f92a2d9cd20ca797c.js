f1b2febd8846b03ff68f376cc2163996546d85d4
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-16 10:19:29 +0100

    Updates

---
+++ precache-manifest.fe8a65170a63ac5f92a2d9cd20ca797c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0aa55ddaf8a75ceabb123813412e4c",
+    "url": "/TODOREDUX/index.html"
+  },
+  {
+    "revision": "bdcec3258b72afd7fa37",
+    "url": "/TODOREDUX/static/css/main.548ea6dd.chunk.css"
+  },
+  {
+    "revision": "9c9f5324d363d1e7d521",
+    "url": "/TODOREDUX/static/js/2.9791878d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/TODOREDUX/static/js/2.9791878d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdcec3258b72afd7fa37",
+    "url": "/TODOREDUX/static/js/main.5dca65b2.chunk.js"
+  },
+  {
+    "revision": "fe3e2ba8220fb476d310",
+    "url": "/TODOREDUX/static/js/runtime-main.108312a8.js"
+  }
+]);
